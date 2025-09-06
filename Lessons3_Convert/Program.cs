e88e159cad74ab3b1aaf6e290d7d3483496ee56e
Nikita  Vakarenko <138662704+usernamedudh@users.noreply.github.com>
2025-09-06 23:43:08 +0300

    Add project files.

---
+++ Lessons3_Convert/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lessons3_Convert
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string str;
+
+            int a, b,c;
+
+            Console.Write("Enter the number1:");
+            str = Console.ReadLine();
+            a = Convert.ToInt32(str);
+
+            Console.Write("Enter the number2:");
+            str = Console.ReadLine();
+            b = Convert.ToInt32(str);
+
+            int result = a + b;
+            Console.WriteLine("The sum is:" + result);
+
+
+            Console.Write("Enter the double:");
+            str = Console.ReadLine();
+            NumberFormatInfo numberFormatInfo = new NumberFormatInfo()
+            {
+                NumberDecimalSeparator = ".",
+            };
+            double d = Convert.ToDouble(str,numberFormatInfo);
+            Console.WriteLine(d);
+            Console.ReadKey();
+        }
+    }
+}
